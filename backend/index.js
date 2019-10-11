c6d704730e729e7e5c762b81ee7f3b3f68f4a41c
Briggs Elsperger <briggse@protonmail.com>
2019-10-11 17:51:07 -0500

    set up mongo connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,25 @@
+// Load environment variables before anything
+require('dotenv').config({
+  path: '.env'
+});
+
+// Establish DB connection before starting application
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 1000
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.error(err.message);
+})
+
 const app = require('./app');
-const port = 3000;
+const port = process.env.PORT;
 
 const server = app.listen(port, () => {
   console.log(`Express running -> PORT ${server.address().port}`);
