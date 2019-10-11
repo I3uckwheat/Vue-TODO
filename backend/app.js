fd84434057c8bf9db6f8fab60c39ed6fc0b55621
Briggs Elsperger <briggse@protonmail.com>
2019-10-11 18:41:30 -0500

    sets up app for authentication and sessions

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+// Local dependencies
 const routes = require('./routes')
+
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
 
