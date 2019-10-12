5c891df8809705ebbb27e8d4c52c495513a3d762
Briggs Elsperger <briggse@protonmail.com>
2019-10-11 21:15:51 -0500

    remove unused code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,6 @@ router.get('/', (req, res) => {
   res.send('hello world');
 })
 
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
 router.post('/user', UserController.register); 
+
 module.exports = router;
