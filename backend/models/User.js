7c060ffb3ab4f838fb5f292c4c932cf94acd9a4e
Briggs Elsperger <briggse@protonmail.com>
2019-10-12 03:00:26 -0500

    Adds slugs to User model for Todos

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const slug = require('slugs');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const Todo = new Schema({
   entered: {type: Date, default: Date.now()},
-  due: Date,
   title: String,
+  slug: {type: String, unique: true},
   body: String,
+  due: Date,
   completed: {type: Boolean, default: false},
   completedDate: Date
 });
@@ -17,4 +19,27 @@ const User = new Schema({
 
 User.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', User);
+const UserModel = mongoose.model('User', User);
+Todo.pre('save', async function(next) {
+  if(!this.isModified()) {
+    return next();
+  }
+
+  this.slug = slug(this.title);
+  const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const user = await UserModel.findOne({username: this.parent().username});
+
+  const duplicateSlugCount = user.todos.reduce((acc, todo) => {
+    if (slugRegex.test(todo.slug)) acc++;
+    return acc;
+  }, 0);
+
+  if (duplicateSlugCount) {
+    this.slug = `${this.slug}-${duplicateSlugCount + 1}`;
+  }
+
+  return next();
+});
+
+
+module.exports = UserModel;
