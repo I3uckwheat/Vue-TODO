5494db346a9ac0204e4e0fdc4eb15bc2187cc2ed
Briggs Elsperger <briggse@protonmail.com>
2019-10-12 00:46:47 -0500

    refactor routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const UserController = require('../controllers/UserController')
 
 const userRoutes = require("./UserRoutes");
+
+router.post('/register', UserController.register); 
+
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  res.sendStatus(200);
+});
 
 router.use(userRoutes)
 
