d97873bbdac319b0d23e84b506574a14df5d258f
Briggs Elsperger <briggse@protonmail.com>
2019-10-12 01:11:47 -0500

    adds todos to schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new Schema({});
+const Todo = new Schema({
+  entered: {type: Date, default: Date.now()},
+  due: Date,
+  title: String,
+  body: String,
+  completed: {type: Boolean, default: false},
+  completedDate: Date
+});
+
+const User = new Schema({
+  todos: [Todo]
+});
+
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
