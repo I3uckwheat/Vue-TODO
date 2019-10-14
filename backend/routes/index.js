014dfc864f6ed2f1e18984ee9db41b138fbe1ac2
Briggs Elsperger <briggse@protonmail.com>
2019-10-14 17:12:10 -0500

    adds header denoting auth status on each request

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const userRoutes = require("./UserRoutes");
 const todoRoutes = require("./TodoRoutes");
 
+// Easy way to check authentication on every request on the front-end in case the client has multiple tabs open 
+// and signs out of one, also for initial authentication state
+router.use((req, res, next) => {
+  res.set("Is-Authenticated", req.isAuthenticated());
+  next();
+});
+
 router.use('/user', userRoutes)
 router.use('/todos', todoRoutes)
 
