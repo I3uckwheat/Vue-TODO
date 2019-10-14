190ddca3f2cfd297f2740810d17d315450ae941d
Briggs Elsperger <briggse@protonmail.com>
2019-10-14 18:42:02 -0500

    adds login callback

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,8 +12,10 @@ module.exports.getUser = async (req, res, next) => {
 module.exports.register = async (req, res, next) => {
   try {
     const user = await User.register(new User({username: req.body.username}), req.body.password);
-    req.login(user);
-    res.sendStatus(201);
+    req.login(user, err => {
+      if (err) return next(err);
+      res.sendStatus(201);
+    });
   } catch(err) {
     res.status(401).send(err);
     next(err);
