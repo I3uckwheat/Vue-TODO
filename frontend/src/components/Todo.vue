00255e61eabb0d61aabd361a3055269480563966
Briggs Elsperger <briggse@protonmail.com>
2019-10-16 11:14:42 -0500

    adds loading bar for todos

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="todo-container">
+    <div class="mask" v-if="loading">
+      <loader />
+    </div>
     <div class="todo-controls">
-      <button v-if="!todo.completed" @click="$store.dispatch('completeTodo', todo.slug)">Complete</button>
-      <button v-else @click="$store.dispatch('unCompleteTodo', todo.slug)">Un-Complete</button>
-      <router-link :to="`/${todo.slug}/edit`"><button>Edit</button></router-link>
+      <button v-if="!todo.completed" @click="completeTodo" :disabled="loading">Complete</button>
+      <button v-else @click="unCompleteTodo">Un-Complete</button>
+      <router-link :to="`/${todo.slug}/edit`" :disabled="loading"><button>Edit</button></router-link>
     </div>
 
     <div class="todo-data">
@@ -17,10 +20,31 @@
 
 <script>
 // @ is an alias to /src
+import Loader from "./Loader"
 
 export default {
   name: 'todo',
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    async completeTodo() {
+      if(this.loading) return;
+      this.loading = true
+      await this.$store.dispatch('completeTodo', this.todo.slug)
+      this.loading = false
+    },
+    async unCompleteTodo() {
+      if(this.loading) return;
+      this.loading = true
+      await this.$store.dispatch('unCompleteTodo', this.todo.slug)
+      this.loading = false
+    }
+  },
   components: {
+    Loader
   },
   props: ["todo"],
 }
@@ -30,5 +54,19 @@ export default {
 .todo-container {
   border: 1px solid black;
   margin-bottom: 80px;
+  position: relative;
+}
+
+.mask {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
