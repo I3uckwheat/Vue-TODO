a932992ea81862498ee283817106dbdd6216a8c4
Briggs Elsperger <briggse@protonmail.com>
2019-10-16 12:00:00 -0500

    sets up basic layout for header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <h1>Vue Todo!</h1>
-    <loader v-if="!$store.state.initialized" type='ring'/>
-    <router-view />
+    <header :class="{'logged-out': !$store.state.isAuthenticated}">
+      <h1>Vue Todo!</h1>
+      <div class="user" v-if="$store.state.username">
+        <p>Hello <span class="username">{{$store.state.username}},</span></p>
+        <p>Not you? Click <button class="logout-btn" @click="$store.dispatch('logout')">here to log out</button>.</p>
+      </div>
+    </header>
+    <body>
+      <loader v-if="!$store.state.initialized" type='ring'/>
+      <router-view />
+    </body>
   </div>
 </template>
 
@@ -23,8 +31,47 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   display: flex;
+  width: 100%;
+  flex-direction: column;
+}
+
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+header.logged-out {
+  justify-content: center;
+}
+
+header .user {
+  line-height: 2px;
+}
+
+header .username {
+  text-transform: uppercase;
+}
+
+header .logout-btn {
+  box-shadow: none;
+  background: none;
+  padding: 0;
+  border: none;
+  color: #003e6b;
+  font-weight: bold;
+}
+
+header .logout-btn:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+body {
+  display: flex;
   align-items: center;
   flex-direction: column;
-  width: 100%;
 }
+
 </style>
